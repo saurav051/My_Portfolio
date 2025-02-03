7046917326f744287acc42e67e1cdd3a1d43394b
saurav051 <55019445+saurav051@users.noreply.github.com>
2025-02-03 17:16:26 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+/* script.js */
+
+// Optional: Add interactivity here if needed
+document.addEventListener("DOMContentLoaded", function() {
+  // You can add JavaScript code here for additional functionality.
+});
